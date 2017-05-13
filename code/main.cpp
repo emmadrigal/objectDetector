19b3deae9ea7ce3ebb5f806dd642af1c9d93c1c6
Emmanuel Madrigal <emma.madri97@gmail.com>
2017-05-12 18:30:49 -0600

    Added display for the matching, but it has to be checked

--- code/main.cpp
+++ code/main.cpp
@@ -61,6 +61,7 @@ void display(){
     cv::DescriptorMatcher* matcher;
     //TODO cahnge this according to the config file
     if(true){
+        //TODO FLANN fails when there are no matches???
         matcher = new cv::FlannBasedMatcher();
     }
     
@@ -68,31 +69,22 @@ void display(){
     cv::Mat traking_descriptor, img_descriptor;
     
     //Object to store the frame
-    cv::Mat frame;
+    cv::Mat frame, tracked_img;
     
-    //TODO this is tmp
+    //Objcet to store the interest points
     std::vector<cv::KeyPoint> tracked_img_key;
-    
-    
     
     for(;;)
     {    
         cap >> frame; // get a new frame from camera        
 
-
-
-        
         //If space bar is pressed it creates the descriptors for the selected image
-        if(cv::waitKey(1) == 32){
-            //Objcet to store the interest points
-            std::vector<cv::KeyPoint> reference_keypoints;
-            
+        if(cv::waitKey(1) == 32){           
             //Takes the keypoints from the image and saves them in the structure
-            detector->detect(frame, reference_keypoints);         
-            
+            detector->detect(frame, tracked_img_key);         
             
             //Extracts the keypoints from the structre
-            extractor->compute(frame, reference_keypoints, traking_descriptor);
+            extractor->compute(frame, tracked_img_key, traking_descriptor);
             
             //Clears previous descriptor
             matcher->clear();
@@ -121,10 +113,43 @@ void display(){
             std::vector< cv::DMatch > matches;
             
             //Matches the detected descriptors with the saved descriptors
-            matcher->match(img_descriptor, traking_descriptor, matches);
+            matcher->match(traking_descriptor, img_descriptor,  matches);
+            
+            
+            //TODO this is coppied from the internet, must be checed
+            double max_dist = 0; double min_dist = 100;
+
+            //-- Quick calculation of max and min distances between keypoints
+            for( int i = 0; i < traking_descriptor.rows; i++ )
+            { double dist = matches[i].distance;
+            if( dist < min_dist ) min_dist = dist;
+            if( dist > max_dist ) max_dist = dist;
+            }
+
+            //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+            //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+            //-- small)
+            //-- PS.- radiusMatch can also be used here.
+            std::vector< cv::DMatch > good_matches;
+
+            //TODO get min value from config file
+            for( int i = 0; i < traking_descriptor.rows; i++ )
+            { if( matches[i].distance <= std::max(2*min_dist, 0.01 ) )
+            { good_matches.push_back( matches[i]); }
+            }
+            
+            //-- Draw only "good" matches
+            cv::Mat img_matches;
+            //TODO get one of this images to be static
+            cv::drawMatches(tracked_img, tracked_img_key, frame, reference_keypoints,
+                       good_matches, img_matches, cv::Scalar::all(-1), cv::Scalar::all(-1) );
+
+            //-- Show detected matches
+            cv::imshow( "Good Matches", img_matches );            
         }
-        
-        cv::imshow("axis", frame);
+        else{
+            cv::imshow("video", frame);
+        }
     }
 }
 
